c37a9a38182623c18ef6cf2b6a79492816361e36
simon marchal <simon.marchal@epitech.eu>
2023-10-06 10:04:41 -0400

    update

--- task04.js
+++ task04.js
@@ -1,89 +1,75 @@
-function showAlert(message, alertType) {
-    const alertDiv = document.createElement('div');
-    alertDiv.classList.add('alert', alertType);
-    alertDiv.innerHTML = message;
-    
-    const container = document.querySelector('.container');
-    container.insertBefore(alertDiv, container.firstChild);
-    // Supprime l'alerte après 5 secondes
-    setTimeout(() => {
-    alertDiv.remove();
-    }, 3000);
-}
+document.addEventListener("DOMContentLoaded", function() {
+    const typeInput = document.getElementById("type");
+    const brandInput = document.getElementById("brand");
+    const typeValidationMessage = document.getElementById("typeValidationMessage");
+    const brandValidationMessage = document.getElementById("brandValidationMessage");
+    const serverResponse = document.getElementById("serverResponse");
 
-function validateType() {
-    const typeInput = document.getElementById('typeInput');
-    const typeValidationMsg = document.getElementById('typeValidationMsg');
-    typeValidationMsg.innerHTML = '';
+    const showAlert = (message, alertClass) => {
+        serverResponse.innerHTML = `
+            <div class="alert ${alertClass} alert-dismissible fade show" role="alert">
+                ${message}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        `;
+    };
 
-    const type = typeInput.value.trim();
+    const validateType = () => {
+        // Your type validation logic here
+        // Example: Check length and valid characters
+        const type = typeInput.value;
+        const validType = /^[a-zA-Z\-]{3,10}$/.test(type);
 
-    if (type.length < 3) {
-    showAlert('$type: this type does not have enough characters.', 'alert-danger');
-    return;
-    }
+        typeValidationMessage.textContent = validType
+            ? "Type is valid"
+            : "Invalid type. Please check length and characters.";
+        typeValidationMessage.className = validType ? "valid" : "error";
+        return validType;
+    };
 
-    if (type.length > 10) {
-    showAlert('$type: this type has too many characters.', 'alert-danger');
-    return;
-    }
+    const validateBrand = () => {
+        // Your brand validation logic here
+        // Example: Check length, valid characters, and if it exists in the database
+        const brand = brandInput.value;
+        const validBrand = /^[a-zA-Z0-9\-&]{2,20}$/.test(brand);
 
-    if (!/^[a-zA-Z-]+$/.test(type)) {
-    showAlert('$type: this type has non-alphabetical characters (different from "-").', 'alert-danger');
-    return;
-    }
-    showAlert('$type: this type exists in databases.', 'alert-success');
-}
+        brandValidationMessage.textContent = validBrand
+            ? "Brand is valid"
+            : "Invalid brand. Please check length and characters.";
+        brandValidationMessage.className = validBrand ? "valid" : "error";
+        return validBrand;
+    };
 
-function validateBrand() {
-    const brandInput = document.getElementById('brandInput');
-    const brandValidationMsg = document.getElementById('brandValidationMsg');
-    brandValidationMsg.innerHTML = '';
-    const brand = brandInput.value.trim();
+    typeInput.addEventListener("input", validateType);
+    brandInput.addEventListener("input", validateBrand);
 
-    if (brand.length < 2) {
-        showAlert('$brand: this brand does not have enough characters.', 'alert-danger');
-        return;
-        }
-    if (brand.length > 20) {
-        showAlert('$brand: this brand has too many characters.', 'alert-danger');
-        return;
-        }
+    const brandForm = document.getElementById("brandForm");
+    brandForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+        
+        const isTypeValid = validateType();
+        const isBrandValid = validateBrand();
 
-    if (!/^[a-zA-Z0-9\-&]+$/.test(brand)) {
-        showAlert('$brand: this brand has invalid characters.', 'alert-danger');
-        return;
-        }
-    
-    showAlert('$brand: this brand is valid for the type $type.', 'alert-success');
-    }
-    
-function sendValidationRequest(type, brand) {
-    fetch(`task04.php?type=${type}&brand=${brand}`)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => {
-        const typeValidationMsg = document.getElementById('typeValidationMsg');
-        const brandValidationMsg = document.getElementById('brandValidationMsg');
-
-        if (data.typeValidationMsg) {
-            showAlert(data.typeValidationMsg, 'alert-danger');
-        } else {
-            showAlert('No type sent yet!', 'alert-success');
+        if (!isTypeValid || !isBrandValid) {
+            showAlert("Please fix validation errors.", "alert-danger");
+            return;
         }
 
-        if (data.brandValidationMsg) {
-            showAlert(data.brandValidationMsg, 'alert-danger');
-        } else {
-            showAlert('No brand sent yet!', 'alert-success');
-        }
-        })
-    .catch(error => {
-        console.error('Error:', error);
-        showAlert('An error occurred while validating.', 'alert-danger');
+        // Send a GET request to your server using fetch() to check brand existence
+        // You will need to implement the server-side logic to check the brand in the database
+        // and respond accordingly
+        fetch("task04.php?type=" + encodeURIComponent(typeInput.value) + "&brand=" + encodeURIComponent(brandInput.value))
+            .then(response => response.json())
+            .then(data => {
+                if (data.exists) {
+                    showAlert("Brand already exists in the database.", "alert-danger");
+                } else {
+                    showAlert("Brand is valid for the type " + data.type + ".", "alert-success");
+                }
+            })
+            .catch(error => {
+                showAlert("An error occurred while checking the brand.", "alert-danger");
+                console.error(error);
+            });
     });
-}
+});
--- task04.php
+++ task04.php
@@ -1,21 +1,50 @@
 <?php
-$conn = new mysqli('localhost', 'root', '', 'ajax_products');
+// Configuration de la connexion à la base de données
+$host = "localhost"; // Adresse du serveur MySQL (peut varier)
+$database = "ajax_products"; // Nom de la base de données
+$username = "root"; // Nom d'utilisateur MySQL
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+
+try {
+    // Connexion à la base de données MySQL
+    $pdo = new PDO("mysql:host=$host;dbname=$database;charset=utf8", $username);
+    
+    // Récupérer les valeurs du type et de la marque depuis la requête GET
+    $type = isset($_GET['type']) ? $_GET['type'] : '';
+    $brand = isset($_GET['brand']) ? $_GET['brand'] : '';
+
+    // Valider le type et la marque (ajoutez votre logique de validation ici)
+    $validType = preg_match('/^[a-zA-Z\-]{3,10}$/', $type);
+    $validBrand = preg_match('/^[a-zA-Z0-9\-&]{2,20}$/', $brand);
+
+    if (!$validType || !$validBrand) {
+        // Si le type ou la marque ne sont pas valides, renvoyer une réponse d'erreur
+        $response = array("error" => true, "message" => "Invalid type or brand.");
+        http_response_code(400); // Bad Request
+    } else {
+        // Vérifier si la marque existe déjà dans la base de données
+        $stmt = $pdo->prepare("SELECT COUNT(*) FROM products WHERE type = :type AND brand = :brand");
+        $stmt->bindParam(':type', $type, PDO::PARAM_STR);
+        $stmt->bindParam(':brand', $brand, PDO::PARAM_STR);
+        $stmt->execute();
+        $count = $stmt->fetchColumn();
+
+        if ($count > 0) {
+            // La marque existe déjà
+            $response = array("exists" => true, "type" => $type);
+        } else {
+            // La marque est valide pour le type spécifié
+            $response = array("exists" => false, "type" => $type);
+        }
+    }
+
+    // Répondre avec une réponse JSON
+    header('Content-Type: application/json');
+    echo json_encode($response);
+} catch (PDOException $e) {
+    // En cas d'erreur de connexion à la base de données
+    $response = array("error" => true, "message" => "Database error: " . $e->getMessage());
+    http_response_code(500); // Internal Server Error
+    echo json_encode($response);
 }
-
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    $type = $_GET['type'];
-    $brand = $_GET['brand'];
-
-    $response = array(
-    'typeValidationMsg' => 'Your type validation message here',
-    'brandValidationMsg' => 'Your brand validation message here'
-);
-
-header('Content-Type: application/json');
-echo json_encode($response);
-}
-
-$conn->close();
+?>
